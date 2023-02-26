fc9af46a9919640d9026189cf2a12602bf5a1b89
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-02-25 18:39:44 -0800

    Task-1 removed setters

--- src/main/model/Transaction.java
+++ src/main/model/Transaction.java
@@ -23,51 +23,19 @@ public class Transaction {
         return this.date;
     }
 
-    /**
-     *
-     * @MODIFIES: this
-     * @EFFECTS: sets the date field to newDate.
-     */
-    public void setDate(String newDate) {
-        this.date = newDate;
-    }
 
     public String getDetails() {
         return this.details;
     }
 
-    /**
-     *
-     * @MODIFIES: this
-     * @EFFECTS: sets the details field to newDetails.
-     */
-    public void setDetails(String newDetails) {
-        this.details = newDetails;
-    }
 
     public double getAmount() {
         return this.amount;
     }
 
-    /**
-     *
-     * @MODIFIES: this
-     * @EFFECTS: sets the amount field to newAmount.
-     */
-    public void setAmount(double newAmount) {
-        this.amount = newAmount;
-    }
 
     public String getCategory() {
         return this.category;
     }
 
-    /**
-     *
-     * @MODIFIES: this
-     * @EFFECTS: sets the category field to newCategory.
-     */
-    public void setCategory(String newCategory) {
-        this.category = newCategory;
-    }
 }
