96de296a37ac7e567bd868d60ab5bb2767ed40ba
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-03-10 22:29:21 -0800

    Phase2, Fixed checkstyle issues caused by intelliJ

--- src/main/model/Transaction.java
+++ src/main/model/Transaction.java
@@ -54,8 +54,12 @@ public class Transaction {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Transaction that = (Transaction) o;
         return date.equals(that.date);
     }
